c87830477880d5455c5110764afc0b2b7c7f78ae
Reve <thereve@gmail.com>
2012-06-07 10:36:01 +0200

    Initial Commit

--- fakeDns.py
+++ fakeDns.py
@@ -23,8 +23,8 @@ class dnsServer(SocketServer.BaseRequestHandler):
 		socket = self.request[1]
 		print "Connect to server: "+self.client_address[0]
 		dominio=""
-		tipo = (ord(data[2]) >> 3) & 15   # Opcode bits
-		if tipo == 0:                     # Standard query
+		tipo = (ord(data[2]) >> 3) & 15   
+		if tipo == 0:                     
 			ini=12
 			lon=ord(data[ini])
 		while lon != 0:
@@ -43,14 +43,13 @@ class dnsServer(SocketServer.BaseRequestHandler):
 			ip=fake_ip(dominio[:-1],fake)
 		
 			packet+=data[:2] + "\x81\x80"
-			packet+=data[4:6] + data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
-			packet+=data[12:]                                         # Original Domain Name Question
-			packet+='\xc0\x0c'                                             # Pointer to domain name
-			packet+='\x00\x01\x00\x01\x00\x00\x00\x01\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
-			packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
-			#print packet
+			packet+=data[4:6] + data[4:6] + '\x00\x00\x00\x00' 
+			packet+=data[12:]                                  
+			packet+='\xc0\x0c'                                 
+			packet+='\x00\x01\x00\x01\x00\x00\x00\x01\x00\x04' 
+			packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) 
 			print strftime("%d-%m-%Y,%H:%M:%S", gmtime())+" Request: %s and response with -> %s" % (dominio[:-1], ip)
-		#dns	
+			
 		logFile.write(strftime("%d-%m-%Y,%H:%M:%S", gmtime())+"| Ip: "+self.client_address[0]+" Richiesta: "+dominio[:-1]+" Ip Risolto: "+ip+"\n")
 		socket.sendto(packet, self.client_address)
 		
